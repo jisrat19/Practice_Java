3267a38026553f5ee82f39c45c1ea5212cda3556
jisrat19 <jisrat19@gmail.com>
2022-04-07 20:56:41 -0400

    Second Project

---
+++ src/infojava/ClassHello.java
@@ -0,0 +1,5 @@
+package infojava;
+
+public class ClassHello {
+
+}
---
+++ src/infojava/ClassHello2.java
@@ -0,0 +1,7 @@
+package infojava;
+
+public class ClassHello2 {
+	public static void main(String [] args) {
+		System.err.println("Hello 2");
+	}
+}
